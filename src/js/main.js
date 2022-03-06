75d2e26f20a65c42b61a1d8c0d4615d9ce179b77
Johanna Skinnari <jskinnari@gmail.com>
2022-03-06 19:30:50 +0100

    background color on links show on selected link

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,12 @@ let home = document.getElementById('home');
 let about = document.getElementById('about');
 let portfolio = document.getElementById('portfolio');
 let contact = document.getElementById('contact');
-let navigation = document.getElementById('navigation');
-let menuButton = document.getElementById('menu-button');
+
+let homeLink = document.getElementById('home-link');
+let aboutLink = document.getElementById('about-link');
+let portfolioLink = document.getElementById('portfolio-link');
+let contactLink = document.getElementById('contact-link');
+
 
 
 let mainRouter = async function (route) {
@@ -13,26 +17,45 @@ let mainRouter = async function (route) {
     portfolio.classList.add('hidden');
     contact.classList.add('hidden');
     about.classList.remove('hidden');
+
+    aboutLink.classList.add('background-primary');
+    homeLink.classList.remove('background-primary');
+    portfolioLink.classList.remove('background-primary');
+    contactLink.classList.remove('background-primary');
   }
   else if (route == '#menu_portfolio') {
     home.classList.add('hidden');
     about.classList.add('hidden');
     contact.classList.add('hidden');
     portfolio.classList.remove('hidden');
+
+    portfolioLink.classList.add('background-primary');
+    homeLink.classList.remove('background-primary');
+    aboutLink.classList.remove('background-primary');
+    contactLink.classList.remove('background-primary');
   }
   else if (route == '#menu_contact') {
     home.classList.add('hidden');
     about.classList.add('hidden');
     portfolio.classList.add('hidden');
     contact.classList.remove('hidden');
+
+    contactLink.classList.add('background-primary');
+    homeLink.classList.remove('background-primary');
+    aboutLink.classList.remove('background-primary');
+    portfolioLink.classList.remove('background-primary');
   }
   else {
     about.classList.add('hidden');
     portfolio.classList.add('hidden');
     contact.classList.add('hidden');
     home.classList.remove('hidden');
+
+    homeLink.classList.add('background-primary');
+    aboutLink.classList.remove('background-primary');
+    portfolioLink.classList.remove('background-primary');
+    contactLink.classList.remove('background-primary');
   }
-  // navigation.classList.toggle('mobile-hide');
 }
 
 async function routFunction(params) {
@@ -44,8 +67,5 @@ window.addEventListener("hashchange", function () {
   routFunction();
 });
 
-// menuButton.addEventListener('click', function () {
-//   navigation.classList.toggle('mobile-hide');
-// });
 
 
